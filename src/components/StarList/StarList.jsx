fe0ca383939647586dc37075234231b59a7652b2
Osyfliak Ivan <osuflak22@gmail.com>
2023-02-07 15:47:51 +0200

    123

---
+++ src/components/StarList/StarList.jsx
@@ -0,0 +1,12 @@
+const StarList = ({ name, gender }) => {
+  return (
+    <ul>
+      <li>
+        <p>Name:{name}</p>
+        <p>Name:{gender}</p>
+      </li>
+    </ul>
+  );
+};
+
+export default StarList;
